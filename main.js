771ebadad26da579d58899929a119d3d6a2920b8
Maicom Stifini <aikobina10@gmail.com>
2024-04-29 12:50:50 -0300

    fix: limitacao de casas decimais

--- main.js
+++ main.js
@@ -47,7 +47,7 @@ function atualizaTabela (){
 function atualizaMediaFInal() {
     const mediafinal = calculaMediaFinal();
 
-    document.getElementById('media-final-valor').innerHTML = mediafinal;
+    document.getElementById('media-final-valor').innerHTML = mediafinal.toFixed(2);
     document.getElementById('media-final-resultado').innerHTML = mediafinal >= notaMinina ? spanAprovado : spanReprovado;
 }
 
